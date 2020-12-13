1c64ede506f015100b52a8758ed7546afe29b26f
junfan0126 <1139851382@qq.com>
2020-12-13 14:45:14 +0800

    704、二分查找及拓展

--- src/main/java/leetcode/BinarySearch.java
+++ src/main/java/leetcode/BinarySearch.java
@@ -4,7 +4,7 @@ package leetcode;
  * @author
  * @version 1.0
  * @date 2020/12/13 下午1:59
- * @description     704、二分查找
+ * @description     704、二分查找及拓展
  **/
 
 public class BinarySearch {
@@ -26,5 +26,57 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    //寻找左侧边界的二分搜索
+    public int search1(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        while (left<=right){
+            //两边界都是闭区间
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                //收缩有边界，降低区域上线
+                right=mid-1;
+            }
+            else if(nums[mid]<target){
+                //收缩左边界
+                left=mid+1;
+            }else if(nums[mid]>target){
+                //收缩右边界
+                right=mid-1;
+            }
+        }
+        if(left>=nums.length||nums[left]!=target){
+            return -1;
+        }
+        return left;
+    }
+
+    //寻找右侧边界的二分搜索
+    public int search2(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        while (left<=right){
+            //两边界都是闭区间
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                //收缩左边界，提高区域上线
+                left=mid+1;
+            }
+            else if(nums[mid]<target){
+                //收缩左边界
+                left=mid+1;
+            }else if(nums[mid]>target){
+                //收缩右边界
+                right=mid-1;
+            }
+        }
+        //检查出界情况
+        //当target比所有元素都小时，right会被减到-1
+        if(right<0||nums[right]!=target){
+            return -1;
+        }
+        return right;
     }
 }
